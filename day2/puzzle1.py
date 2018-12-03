e0eed1071e9f5ffb0774b15fe5b237a690c71922
Wes Novack <wes-novack@pluralsight.com>
2018-12-02 20:44:54 -0700

    rm unneeded print statements

--- day2/puzzle1.py
+++ day2/puzzle1.py
@@ -7,8 +7,6 @@ def check_duplicate_chars(string):
     for letter in string:
         if letter in character_dict:
             character_dict[letter] += 1
-            print(letter)
-            print(character_dict[letter])
         else:
             character_dict[letter] = 1
     duplicate_letter_nums = set()
@@ -19,8 +17,6 @@ def check_duplicate_chars(string):
 
 
 def calculate_hash():
-    print("Twos: {}".format(twos))
-    print("Threes: {}".format(threes))
     print("Hash = {}".format(twos * threes))
 
 
