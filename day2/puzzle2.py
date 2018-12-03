7734d3dfb09857a227a012d824bf6f45d37500b2
Wes Novack <wes-novack@pluralsight.com>
2018-12-03 00:07:06 -0700

    completed day2 puzzle 2

--- day2/puzzle2.py
+++ day2/puzzle2.py
@@ -13,11 +13,36 @@ def calculate_common_letters(string1, string2):
         if letter == string2[index]:
             common_letters += letter
         index += 1
-    print(common_letters)
     return common_letters
+
+
+def find_similar(in_list):
+    list_length = len(in_list)
+    for id in in_list:
+        for index in range(list_length):
+            if compare_two(id, in_list[index]):
+                return [id, in_list[index]]
+            else:
+                continue
+
+
+def compare_two(string1, string2):
+    if string1 == string2:
+        return None
+    index = 0
+    mismatch = 0
+    for letter in string1:
+        if mismatch > 1:
+            return None
+        if letter == string2[index]:
+            index += 1
+        else:
+            mismatch += 1
+            index += 1
+    return [string1, string2]
 
 
 if __name__ == "__main__":
     ids = read_file()
-    for line in ids:
-        pass
+    id1, id2 = find_similar(ids)
+    print(calculate_common_letters(id1, id2))
--- day2/test_puzzle2.py
+++ day2/test_puzzle2.py
@@ -2,12 +2,33 @@ from day2.puzzle2 import *
 import pytest
 
 calculate_common_letters_testdata = [
-    ("abcde","abcdf","abcd"),
+    ("abcdeefgh","abcdfefgh","abcdefgh"),
     ("fghij","fguij","fgij")
-#    (["abcde","fghij","klmno","pqrst","fguij","axcye","wvxyz"], "fgij"),
 ]
+
 
 @pytest.mark.parametrize("string1,string2,common_letters", calculate_common_letters_testdata)
 def test_calculate_common_letters(string1, string2, common_letters):
     assert calculate_common_letters(string1, string2) == common_letters
 
+
+find_similar_testdata = [
+    (["abcde","fghij","klmno","pqrst","fguij","axcye","wvxyz"], ["fghij","fguij"]),
+]
+
+
+@pytest.mark.parametrize("in_list,out_list", find_similar_testdata)
+def test_find_similar(in_list, out_list):
+    assert find_similar(in_list) == out_list
+
+
+compare_two_testdata = [
+    ("fghij","fguij", ["fghij", "fguij"]),
+    ("abcde","fghij", None),
+    ('abcde', 'axcye', None)
+]
+
+
+@pytest.mark.parametrize("string1,string2,answer", compare_two_testdata)
+def test_compare_two(string1, string2, answer):
+    assert compare_two(string1, string2) == answer
