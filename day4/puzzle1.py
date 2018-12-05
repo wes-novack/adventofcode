6232d78917146a708af9e945a6cc4684bc265984
Wes Novack <wes-novack@pluralsight.com>
2018-12-05 00:27:31 -0700

    day4 puzzle1 solution

--- day4/puzzle1.py
+++ day4/puzzle1.py
@@ -3,9 +3,11 @@
 #2) find the Guard# with the most minutes of sleep
 #3) for the Guard# with the most minutes of sleep, what minute does that guard spend asleep the most?
 #4) What is the ID of the guard you chose multiplied by the minute you chose?
+import re
 
-def read_file():
-    with open("input.txt") as file:
+
+def read_file(filename):
+    with open(filename) as file:
         log_lines = []
         for line in file:
             log_lines.append(line)
@@ -13,6 +15,74 @@ def read_file():
         return log_lines
 
 
-if __name__ == '__main__':
-    read_file()
+def find_guard_that_sleeps_most(lines):
+    guards_total_sleep = {}
+    for line in lines:
+        if 'begins shift' in line:
+            guard_id = line.split()[3]
+            continue
+        elif 'falls asleep' in line:
+            time = re.search('\d{2}:\d{2}(?=] )',line)
+            start_time = time.group(0).replace("00:", "")
+        elif 'wakes up' in line:
+            time = re.search('\d{2}:\d{2}(?=] )',line)
+            end_time = time.group(0).replace("00:", "")
+            sleep_time = int(end_time) - int(start_time)
+            if guard_id in guards_total_sleep:
+                guards_total_sleep[guard_id] += sleep_time
+            else:
+                guards_total_sleep[guard_id] = sleep_time
+        else:
+            print("No if blocks matched for line")
+    guard_with_most_sleep = ""
+    otherkey, othervalue = next(iter(guards_total_sleep.items()))
+    for k, v in guards_total_sleep.items():
+        if v > othervalue:
+            guard_with_most_sleep = k
+            otherkey, othervalue = k, v
+        else:
+            guard_with_most_sleep = otherkey
+    return guard_with_most_sleep
 
+
+def most_popular_min_of_sleep_for_guard(guard_id, lines):
+    minute_dict = {}
+    guard_id_found = False
+    for line in lines:
+        if guard_id in line:
+            guard_id_found = True
+            continue
+        elif 'falls asleep' in line and guard_id_found == True:
+            time = re.search('\d{2}:\d{2}(?=] )',line)
+            start_time = time.group(0).replace("00:", "")
+        elif 'wakes up' in line and guard_id_found == True:
+            time = re.search('\d{2}:\d{2}(?=] )',line)
+            end_time = time.group(0).replace("00:", "")
+            for min in range(int(start_time), int(end_time)):
+                if min in minute_dict:
+                    minute_dict[min] += 1
+                else:
+                    minute_dict[min] = 1
+        else:
+            guard_id_found = False
+
+    most_popular_minute = ""
+    otherkey, othervalue = next(iter(minute_dict.items()))
+    for k, v in minute_dict.items():
+        if int(v) > int(othervalue):
+            most_popular_minute = k
+            otherkey, othervalue = k, v
+        else:
+            most_popular_minute = otherkey
+    return most_popular_minute
+
+
+if __name__ == '__main__':
+    lines = read_file("input.txt")
+    guard_with_most_sleep = find_guard_that_sleeps_most(lines)
+    minute = most_popular_min_of_sleep_for_guard(guard_with_most_sleep, lines)
+    print("minute: {}".format(minute))
+    guard = guard_with_most_sleep.replace("#", "")
+    print("guard: {}".format(guard))
+    puzzle_answer = int(guard) * minute
+    print(puzzle_answer)
--- day4/test_puzzle1.py
+++ day4/test_puzzle1.py
@@ -1,2 +1,20 @@
 from day4.puzzle1 import *
 import pytest
+
+
+lines = read_file("input_partial.txt")
+
+
+'''
+#137:   16
+#3251:  29 + 26 = 55
+#479:   13 + 17 = 30
+'''
+
+
+def test_find_guard_that_sleeps_most():
+    assert find_guard_that_sleeps_most(lines) == "#3251"
+
+
+def test_most_popular_min_of_sleep_for_guard():
+    assert most_popular_min_of_sleep_for_guard("#3251", lines) == 33
