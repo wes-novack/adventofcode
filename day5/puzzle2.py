b984aa76d9cbd4ca3c589f709b4fb3cec40e2a10
Wes Novack <wes-novack@pluralsight.com>
2018-12-07 08:05:06 -0700

    day5 puzzle2 initial commit

---
+++ day5/puzzle2.py
@@ -0,0 +1,54 @@
+def read_file(filename):
+    with open(filename) as file:
+        input_polymer = file.readline()
+        input_polymer = input_polymer.replace("\n", "")
+        return input_polymer
+
+
+def reduce_polymer(polymer):
+    elements_to_delete = []
+    marked_for_deletion = True
+    while marked_for_deletion:
+        element = 0
+        for char in polymer:
+            max_index = len(polymer) - 1
+            if element >= max_index:
+                break
+            next_char = polymer[element + 1]
+            next_element = element + 1
+            if char.lower() == next_char.lower() and char != next_char:
+                elements_to_delete.append(element)
+                elements_to_delete.append(next_element)
+            else:
+                element += 1
+        if elements_to_delete:
+            polymer = delete_chars(elements_to_delete, polymer)
+            elements_to_delete = []
+        else:
+            marked_for_deletion = False
+    strpolymer = ''.join(polymer)
+    return strpolymer
+
+
+def delete_chars(elements_to_delete, polymer):
+    elements_to_delete.reverse()
+    polymer = list(polymer)
+    for element in elements_to_delete:
+        del polymer[element]
+    return polymer
+
+
+def remove_units_and_compare(input_polymer):
+    units_found = set()
+    for unit in input_polymer:
+        if not unit.lower() in units_found:
+            units_found.add(unit.lower())
+    return units_found
+
+
+if __name__ == "__main__":
+    input_polymer = read_file("input.txt")
+    units_found = remove_units_and_compare(input_polymer)
+    print(units_found)
+    # new_polymer = reduce_polymer(input_polymer)
+    # print(len(new_polymer))
