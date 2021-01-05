ea2cd9d883df13023190df64ae352c1506aa9390
Wes Novack <wes-novack@users.noreply.github.com>
2021-01-04 23:06:46 -0700

    solve 2020/day7/puzzle1

--- 2020/day7/puzzle1.py
+++ 2020/day7/puzzle1.py
@@ -1,15 +1,54 @@
-import re
-
-
 def create_bag_definition(rule):
-    rule_list = rule.split(" bags contain ")
-    top_level_bag = rule_list[0]
     bag_definition = {}
-    bag_definition[top_level_bag] = {}
-    bags_contained = rule_list[1].split(", ")
-    for bag in bags_contained:
-        count = int(bag[0])
-        bag_words = bag.split()
-        bag_identifier = bag_words[1] + " " + bag_words[2]
-        bag_definition[top_level_bag][bag_identifier] = count
+    if not "no other bags" in rule:
+        rule_list = rule.split(" bags contain ")
+        top_level_bag = rule_list[0]
+        bag_definition[top_level_bag] = {}
+        bags_contained = rule_list[1].split(", ")
+        for bag in bags_contained:
+            count = int(bag[0])
+            bag_words = bag.split()
+            bag_identifier = bag_words[1] + " " + bag_words[2]
+            bag_definition[top_level_bag][bag_identifier] = count
+    else:
+        rule_words = rule.split()
+        top_level_bag = rule_words[0] + " " + rule_words[1]
+        bag_definition[top_level_bag] = {}
     return bag_definition
+
+
+def read_file(filename):
+    with open(filename) as file:
+        rules = file.readlines()
+    return rules
+
+
+def process_rules(filename):
+    rules = read_file(filename)
+    rule_list = []
+    for rule in rules:
+        rule_list.append(create_bag_definition(rule))
+    return rule_list
+
+
+def calculate_valid_bag_colors(rule_list, color, valid_bag_colors):
+    valid_bag_start_count = len(valid_bag_colors)
+    for bag in rule_list:
+        for k, v in bag.items():
+            if color in v and k != color:
+                valid_bag_colors.add(k)
+    set_copy = valid_bag_colors.copy()
+    if len(valid_bag_colors) != valid_bag_start_count:
+        for bag in set_copy:
+            calculate_valid_bag_colors(rule_list, bag, valid_bag_colors)
+    return len(valid_bag_colors)
+
+
+def main(filename):
+    rule_list = process_rules(filename)
+    valid_bag_colors = set()
+    return calculate_valid_bag_colors(rule_list, "shiny gold", valid_bag_colors)
+
+
+if __name__ == '__main__':
+    print(main("input.txt"))
--- 2020/day7/test_puzzle1.py
+++ 2020/day7/test_puzzle1.py
@@ -13,3 +13,21 @@ def test_dark_orange():
     actual = create_bag_definition("dark orange bags contain 3 bright white bags, 4 muted yellow bags.")
     expected = {"dark orange": {"bright white": 3, "muted yellow": 4}}
     assert actual == expected
+
+
+def test_faded_blue():
+    actual = create_bag_definition("faded blue bags contain no other bags.")
+    expected = {"faded blue": {}}
+    assert actual == expected
+
+
+def test_bright_white():
+    actual = create_bag_definition("bright white bags contain 1 shiny gold bag.")
+    expected = {"bright white": {"shiny gold": 1}}
+    assert actual == expected
+
+
+def test_sample_input():
+    actual = main("test_input.txt")
+    expected = 4
+    assert actual == expected
